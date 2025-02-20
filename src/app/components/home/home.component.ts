64ca30a41544452cf58f414f9fac50c99f95da81
Quarelli George <quarelli.george@gmail.com>
2025-02-20 23:43:07 +0100

    aggiunta parte user ai post

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -39,4 +39,13 @@ export class HomeComponent implements OnInit {
       console.error('Errore User:', error);
     }
   }
+  
+  getUserByPostId(postId: number): User | undefined {
+    return this.users.find(user => user.id === postId);
+  }
+  
+  getInitials(name: string): string {
+    const names = name.split(' ');
+    return (names[0][0] + names[1][0]).toUpperCase();
+  }
 }
